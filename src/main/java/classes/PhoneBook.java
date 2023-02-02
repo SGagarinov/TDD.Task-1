1b0bbd34918026aaf736c4215c6358a3327d71b5
Abriial <codjumper@mail.ru>
2023-02-02 17:02:13 +0300

    add implemented

--- src/main/java/classes/PhoneBook.java
+++ src/main/java/classes/PhoneBook.java
@@ -1,12 +1,17 @@
 package classes;
 
+import java.util.HashMap;
+
 public class PhoneBook {
+
+    HashMap<String, Long> contacts = new HashMap<>();
 
     public PhoneBook() {
 
     }
 
     public int add(String name, Long number) {
-        return 0;
+        contacts.put(name, number);
+        return contacts.size();
     }
 }
